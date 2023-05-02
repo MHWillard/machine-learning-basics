4ba261a4f32574727722dfe42e4d51678ae840ac
MHWillard <mhwillard@gmail.com>
2023-05-02 16:22:11 -0400

    Setting up for exercises and quick tutorials

--- exercises/numpy_quick_tutorial.py
+++ exercises/numpy_quick_tutorial.py
@@ -9,3 +9,7 @@ print(label)
 # modify each value assigned to label by adding a different random floating-point value between -2 and +2.
 # Don't rely on broadcasting. Instead, create a noise array having the same dimension as label.
 
+noise = (np.random.random([15]) * 4) - 2
+print(noise)
+label = label + noise 
+print(label)
